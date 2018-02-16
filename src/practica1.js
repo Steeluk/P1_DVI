f6dd537523f9b15c0f5ba16d75b489cccedf8792
Steeluk <magos14>
2018-02-16 14:57:51 +0100

    Avance

--- src/practica1.js
+++ src/practica1.js
@@ -11,20 +11,63 @@ var MemoryGame = MemoryGame || {};
 MemoryGame = function(gs) {
 	this.gs = gs;
 	this.cards = [];
+	this.state = "Memory Game";
 	this.initGame = function(){
-		console.log(this.gs.maps);
+		//console.log(this.gs.maps);
 		var that=this;
 		Object.keys(this.gs.maps).forEach(function(key){
-			that.cards.push(new MemoryGameCard(key));
-			that.cards.push(new MemoryGameCard(key));
+		    if (key !== "back") {
+                that.cards.push(new MemoryGameCard(key));
+                that.cards.push(new MemoryGameCard(key));
+            }
 		});
-		//prueba.sort(function(){return 0.5 - Math.random()}); -> Esta linea te desordena un array
-		//console.log(this.cards);
+		this.cards.sort(function(){return 0.5 - Math.random()}); //-> Esta linea te desordena un array
 		this.loop();
 	};
-	this.draw = function(){};
-	this.loop = function(){};
-	this.onClick = function(cardId){};
+	this.draw = function(){
+        var that=this;
+		gs.drawMessage(this.state);
+		this.cards.forEach(function (card, index) {
+		    card.draw(that.gs, index);
+        })
+	};
+	this.loop = function(){
+	    var that = this;
+        setInterval(function() { that.draw() }, 16);
+        //setInterval(this.draw, 3000);
+    };
+	this.onClick = function(cardId){
+	    var that = this;
+        var clickedCard = this.cards[cardId];
+        var secondCard;
+        var count = 0;
+
+        this.cards.forEach(function (card) {
+            if(card.state === card.states.UP) {
+                secondCard = card;
+                count++;
+            }
+        });
+
+        if(count === 0){
+            clickedCard.flip();
+        }
+        if (count === 1) {
+            clickedCard.flip();
+            if (clickedCard.compareTo(secondCard)) {
+                that.state = "Match Found";
+                secondCard.found();
+                clickedCard.found();
+            }
+            else {
+                that.state = "Try again";
+                setTimeout(function () {
+                    secondCard.flip();
+                    clickedCard.flip();
+                }, 600);
+            }
+        }
+    };
 };
 
 
@@ -42,11 +85,25 @@ MemoryGameCard = function(id) {
 		DOWN: 1,
 		FOUND: 2
 	};
-	this.state = states.DOWN;
-	this.flip = function(){};
-	this.found = function(){};
-	this.compareTo = function(otherCard){};
-	this.draw = function(gs, pos){};
+	this.state = this.states.DOWN;
+	this.flip = function(){
+	    if(this.state === this.states.UP)
+            this.state = this.states.DOWN;
+	    else if(this.state === this.states.DOWN)
+            this.state = this.states.UP;
+    };
+	this.found = function(){
+	    this.state = this.states.FOUND;
+    };
+	this.compareTo = function(otherCard){
+	    return(this.id === otherCard.id);
+    };
+	this.draw = function(gs, pos){
+	    if(this.state === this.states.DOWN)
+            gs.draw("back", pos);
+	    else
+            gs.draw(this.id, pos);
+    };
 };
 
 
